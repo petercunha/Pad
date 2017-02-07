0ab40aad1daddd61660272ef797ce5b6311b572a
Peter Cunha <petercunha8@gmail.com>
2017-02-07 16:50:42 -0500

    Add a pseudo-garbage-colletor to clean up any junk Pads and save memory

--- routes/socket.js
+++ routes/socket.js
@@ -2,31 +2,65 @@ module.exports = function(io) {
   var app = require('express');
   var router = app.Router();
   var welcomeMessage = "Pad created! Share the URL with a friend to edit text in real-time.";
-  var pages = new Map();
+  var pages = new Map(); // Stores Pad data
 
+
+  // Handle WebSocket connections
   io.on('connection', function(socket) {
+
     console.log("A user connected")
 
+    // Handle sync request
     socket.on('sync', function (data) {
       if (pages.has(data.path)) {
-        socket.emit('notify', { content: pages.get(data.path), path: data.path });
+
+        // Page exists, notify with pad data
+        notify(socket, pages.get(data.path), data.path);
         console.log("Paste accessed: " + data.path);
+
       } else {
-        socket.emit('notify', { content: '', path: data.path });
+
+        // Page doesn't exist, notify with new pad
+        notify(socket, '', data.path);
         console.log("Created paste: " + data.path);
+
       }
+
+      // Remove junk objects from Map, save space
+      clearUpMemory();
     });
 
+    // Handle incoming data from user
     socket.on('datagram', function (data) {
+
+      // Update pad data in memory
       pages.set(data.path, data.text);
       var curr = pages.get(data.path);
 
-      console.log(data.text);
+      // Notify everyone of update
+      notifyAll(socket, curr, data.path);
 
-      socket.emit('notify', { content: curr, path: data.path });
-      socket.broadcast.emit('notify', { content: curr, path: data.path });
     });
   });
+
+  // Send update to client
+  function notify(socket, c, p) {
+    socket.emit('notify', { content: c, path: p });
+  }
+
+  // Send update to all clients
+  function notifyAll(socket, c, p) {
+    socket.broadcast.emit('notify', { content: c, path: p });
+  }
+
+  // Clean up memory if Map gets too full
+  function clearUpMemory() {
+    for (var [key, value] of pages) {
+      if (value = "") {
+        pages.delete(key);
+      }
+    }
+  }
 
   /* GET pad by unique id */
   router.get('/:id', function(req, res, next) {
